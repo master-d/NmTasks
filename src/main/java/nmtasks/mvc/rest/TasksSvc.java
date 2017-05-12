b3f4b2af46c35b0b5b9e65e4d061e4a211513494
raricha <goodeveningthisisrobrichards@gmail.com>
2017-05-12 17:06:30 -0500

    initial commit of nmtasks

---
+++ src/main/java/nmtasks/mvc/rest/TasksSvc.java
@@ -0,0 +1,22 @@
+package nmtasks.mvc.rest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class TasksSvc{
+	
+	TasksSvc() { }
+	
+	
+	@RequestMapping(value="servername", method=RequestMethod.GET)
+	public ResponseEntity<String> convert(){
+		String text = "DUCK";
+		return new ResponseEntity<>(text, HttpStatus.OK);
+	}
+	
+}
